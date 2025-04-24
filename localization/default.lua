32fb4453177005b0381a2f54a89bf514ce4833fc
Alex343xd <alex343xd@noreply.codeberg.org>
2025-04-24 15:16:48 +0000

    Upload files to "localization"

---
+++ localization/default.lua
@@ -0,0 +1,239 @@
+-- this is just for CardSleeves
+return {
+    descriptions = {
+        Sleeve = {
+            sleeve_may_orange_sleeve = {
+                name = "Orange Sleeve",
+                text = { "{C:attention}+#1# Hand Size{}" }
+            },
+            sleeve_may_orange_sleeve_alt = {
+                name = "Orange Sleeve",
+                text = { "{C:attention}+#1# Hand Size{}" }
+            },
+			
+			sleeve_may_purple_sleeve = {
+                name = "Purple Sleeve",
+                text = { 
+					"You can select {C:green}1 more card{}",
+					"from {C:attention}Booster Packs{}"
+				}
+            },
+            sleeve_may_purple_sleeve_alt = {
+                name = "Purple Sleeve",
+                text = { 
+					"You can select {C:green}all cards{}",
+					"from {C:attention}Booster Packs{}"
+				}
+            },
+			
+			sleeve_may_pink_sleeve = {
+                name = "Pink Sleeve",
+                text = { 
+					"{X:attention,C:white}X#1#{} Blind size"
+				}
+            },
+            sleeve_may_pink_sleeve_alt = {
+                name = "Pink Sleeve",
+                text = { 
+					"{X:attention,C:white}X#1#{} Blind size",
+					"{X:attention,C:white}X0.5{} Boss Blind size"
+				}
+            },
+			
+			sleeve_may_lime_sleeve = {
+                name = "Lime Sleeve",
+                text = { 
+					"{C:attention}+1{} {C:green}Voucher{} slot in {C:atention}shop{}"
+				}
+            },
+            sleeve_may_lime_sleeve_alt = {
+                name = "Lime Sleeve",
+                text = { 
+					"{C:attention}+1{} {C:green}Voucher{} slot",
+					"in {C:atention}shop{}",
+					"Apply {C:attention}Copper Seal{} to {C:attention}all cards{} in deck",
+					"when {C:attention}blind{} is {C:attention}selected{}",
+				}
+            },
+			
+			sleeve_may_brown_sleeve = {
+                name = "Brown Sleeve",
+                text = { 
+					"{C:attention}Playing cards{} give {X:attention,C:white}X10{} more {C:chips}Chips{}"
+				}
+            },
+            sleeve_may_brown_sleeve_alt = {
+                name = "Brown Sleeve",
+                text = { 
+					"{C:attention}Playing cards{} give {X:attention,C:white}X10{} more {C:chips}Chips{}",
+					"{C:attention}and{} {X:chips,C:white}XChips{} based on their rank"
+				}
+            },
+			
+			sleeve_may_gray_sleeve = {
+                name = "Gray Sleeve",
+                text = { 
+					"All {C:attention}cards{} in deck start with",
+					"a {C:attention}random{} {C:dark_edition}seal{}",
+				}
+            },
+            sleeve_may_gray_sleeve_alt = {
+                name = "Gray Sleeve",
+                text = { 
+					"All {C:attention}cards{} in deck start with",
+					"a {C:attention}random{} {C:dark_edition}seal{}",
+					"When {C:attention}blind{} is {C:attention}selected{}, apply {C:dark_edition}seals{}",
+					"to {C:attention}all cards{} in deck {C:attention}without{} one",
+				}
+            },
+			
+			sleeve_may_turqoise_sleeve = {
+                name = "Turqoise Sleeve",
+                text = { 
+					"All {C:attention}cards{} in {C:attention}shop{} are",
+					"{C:attention}25%{} {C:money}cheaper{}"
+				}
+            },
+            sleeve_may_turqoise_sleeve_alt = {
+                name = "Turqoise Sleeve",
+                text = { 
+					"All {C:attention}cards{} in {C:attention}shop{} are",
+					"{C:attention}25%{} {C:money}cheaper{}",
+					"Bought {C:attention}items{} have a fixed {C:green}1 in 5{} chance",
+					"to be {C:attention}refunded{}"
+				}
+            },
+			
+			sleeve_may_blurple_sleeve = {
+                name = "Blurple Sleeve",
+                text = { 
+					"Start run with a {C:attention}random{}",
+					"{X:mult,C:white}Rare{} {C:attention}joker{}"
+				}
+            },
+            sleeve_may_blurple_sleeve_alt = {
+                name = "Blurple Sleeve",
+                text = { 
+					"Start run with a {C:attention}random{}",
+					"{X:legendary,C:white}Legendary{} {C:attention}joker{}"
+				}
+            },
+			
+			sleeve_may_monochrome_sleeve = {
+                name = "Monochrome Sleeve",
+                text = { 
+					"Start run with {C:attention}5{} {C:dark_edition}Negative{}",
+					"copies of a {C:attention}random{} {X:green,C:white}Uncommon{} {C:attention}joker{}"
+				}
+            },
+            sleeve_may_monochrome_sleeve_alt = {
+                name = "Monochrome Sleeve",
+                text = { 
+					"When {C:attention}Boss Blind{} is {C:attention}selected{},",
+					"create a {C:attention}random{} {C:dark_edition}Negative{}",
+					"{X:uncommon,C:white}Uncommon{} {C:attention}Joker{}"
+				}
+            },
+			
+			sleeve_may_rainbow_sleeve = {
+                name = "Rainbow Sleeve",
+                text = { 
+					"{C:attention}+1{} {C:chips}hand{}, {C:mult}discard{} and {C:attention}hand size{}",
+					"{X:attention,C:white}X0.9{} blind size",
+					"Start with an {C:attention}extra{} {C:money}$10{}",
+					"and a radom {X:mult,C:white}Rare{} {C:attention}joker{}",
+				}
+            },
+            sleeve_may_rainbow_sleeve_alt = {
+                name = "Rainbow Sleeve",
+                text = { 
+					"All {C:attention}cards{} in {C:attention}shop{} are",
+					"{C:attention}25%{} {C:money}cheaper{}",
+					"{C:attention}Playing cards{} give {X:attention,C:white}X10{} more {C:chips}Chips{}",
+					"All {C:attention}cards{} in deck start with",
+					"a {C:attention}random{} {C:dark_edition}seal{}",
+					"You can select {C:green}1 more card{}",
+					"from {C:attention}Booster Packs{}",
+					"Earn {C:money}$1{} per {C:mult}discard{} at the",
+					"{C:attention}end of round{}"
+				}
+            },
+			
+			sleeve_may_aaaa_sleeve = {
+                name = "AAAA Sleeve",
+                text = { 
+					"Start run with {C:attention}AAAA{}",
+					"and a deck of only {C:attention}Aces{}",
+				}
+            },
+            sleeve_may_aaaa_sleeve_alt = {
+                name = "AAAA Sleeve",
+                text = { 
+					"Start run with {C:dark_edition}Acum{}",
+				}
+            },
+			
+			sleeve_may_unstable_sleeve = {
+                name = "Unstable Sleeve",
+                text = { 
+					"Start run with {C:dark_edition}Universal Collapse{}",
+				}
+            },
+            sleeve_may_unstable_sleeve_alt = {
+                name = "Unstable Sleeve",
+                text = { 
+					"{C:spectral}Genesis{} will {C:attention}not{}",
+					"{C:attention}increase Ante{} when used"
+				}
+            },
+			
+			sleeve_may_ethereal_sleeve = {
+                name = "Ethereal Sleeve",
+                text = { 
+					"Start run with a random",
+					"{X:may_ethereal,C:white}Ethereal{} {C:attention}Joker{}",
+					"{C:chips}-2 hands{}, {C:mult}-2 discards{} and {C:attention}-3 joker slots{}",
+				}
+            },
+            sleeve_may_ethereal_sleeve_alt = {
+                name = "Ethereal Sleeve",
+                text = { 
+					"{C:attention}Disables{} the {C:mult}downsides{}",
+					"of {C:dark_edition}Ethereal Deck{}"
+				}
+            },
+			
+			sleeve_may_transcendent_sleeve = {
+                name = "Transcendent Sleeve",
+                text = { 
+					"Start run with a random",
+					"{X:dark_edition,C:white}Transcendent{} {C:attention}Joker{}",
+					"{C:chips}-1 hand{} and {C:mult}discard{}"
+				}
+            },
+            sleeve_may_transcendent_sleeve_alt = {
+                name = "Transcendent Sleeve",
+                text = { 
+					"Start run with an additional random",
+					"{X:may_interdimensional,C:white}Interdimensional{} {C:attention}Joker{}",
+				}
+            },
+			
+			sleeve_may_power_sleeve = {
+                name = "Power Sleeve",
+                text = { 
+					"{C:green}1 in 10{} chance to",
+					"{C:attention}increase{} {C:dark_edition}score operator{} level by {C:attention}1{}",
+					"after {C:attention}hand{} is {C:attention}played{}"
+				}
+            },
+            sleeve_may_power_sleeve_alt = {
+                name = "Power Sleeve",
+                text = { 
+					"When {C:attention}Boss Blind{} is {C:attention}selected{},",
+					"{C:attention}increase{} {C:dark_edition}score operator{} level by {C:attention}1{}",
+				}
+            },
+        }
+    }
+}
